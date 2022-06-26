06a0569d388297e8f7d868ada4931613de4e5555
mech <103256433+Brineeee@users.noreply.github.com>
2022-06-26 13:30:37 +0800

    Create Claim.lua

---
+++ Claim.lua
@@ -0,0 +1,104 @@
+-- Credits to Yamete#2273 for making me this! --
+
+while 
+      true
+           do --// Auto Claim Gifts
+
+local args = {
+    [1] = {
+        [1] = 1,
+    }
+}
+
+workspace.__THINGS.__REMOTES:FindFirstChild("redeem free gift"):InvokeServer(unpack(args))
+
+local args = {
+    [1] = {
+        [1] = 2
+    }
+}
+
+workspace.__THINGS.__REMOTES:FindFirstChild("redeem free gift"):InvokeServer(unpack(args))
+
+local args = {
+    [1] = {
+        [1] = 3
+    }
+}
+
+workspace.__THINGS.__REMOTES:FindFirstChild("redeem free gift"):InvokeServer(unpack(args))
+
+local args = {
+    [1] = {
+        [1] = 4
+    }
+}
+
+workspace.__THINGS.__REMOTES:FindFirstChild("redeem free gift"):InvokeServer(unpack(args))
+
+local args = {
+    [1] = {
+        [1] = 5
+    }
+}
+
+workspace.__THINGS.__REMOTES:FindFirstChild("redeem free gift"):InvokeServer(unpack(args))
+
+local args = {
+    [1] = {
+        [1] = 6
+    }
+}
+
+workspace.__THINGS.__REMOTES:FindFirstChild("redeem free gift"):InvokeServer(unpack(args))
+
+local args = {
+    [1] = {
+        [1] = 7
+    }
+}
+
+workspace.__THINGS.__REMOTES:FindFirstChild("redeem free gift"):InvokeServer(unpack(args))
+
+local args = {
+    [1] = {
+        [1] = 8
+    }
+}
+
+workspace.__THINGS.__REMOTES:FindFirstChild("redeem free gift"):InvokeServer(unpack(args))
+
+local args = {
+    [1] = {
+        [1] = 9
+    }
+}
+
+workspace.__THINGS.__REMOTES:FindFirstChild("redeem free gift"):InvokeServer(unpack(args))
+
+local args = {
+    [1] = {
+        [1] = 10
+    }
+}
+
+workspace.__THINGS.__REMOTES:FindFirstChild("redeem free gift"):InvokeServer(unpack(args))
+
+local args = {
+    [1] = {
+        [1] = 11
+    }
+}
+
+workspace.__THINGS.__REMOTES:FindFirstChild("redeem free gift"):InvokeServer(unpack(args))
+
+local args = {
+    [1] = {
+        [1] = 12
+    }
+}
+
+workspace.__THINGS.__REMOTES:FindFirstChild("redeem free gift"):InvokeServer(unpack(args))
+
+              wait()
+                      end
